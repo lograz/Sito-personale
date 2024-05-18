fafc18c105c0ae0580d69f810c993d810e2160cc
lograz <103561745+lograz@users.noreply.github.com>
2024-05-18 21:00:22 +0200

    Add files via upload
    loader page

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-ScrollReveal().reveal('.card');
+// script.js
+
+document.addEventListener("DOMContentLoaded", () => {
+    // Simulate an API request or any async operation
+    setTimeout(() => {
+        hideLoader();
+        showContent();
+    }, 1000); // Replace with your actual data loading logic and time
+
+    function hideLoader() {
+        const loader = document.getElementById("loader");
+        loader.style.display = "none";
+    }
+
+    function showContent() {
+        const content = document.getElementById("content");
+        content.style.display = "flex";
+    }
+});
+
