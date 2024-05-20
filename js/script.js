8e78fbddab1b6615db020f41add201159474f87f
lograz <103561745+lograz@users.noreply.github.com>
2024-05-20 10:55:23 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,15 +3,11 @@
 document.addEventListener("DOMContentLoaded", () => {
     // Simulate an API request or any async operation
     setTimeout(() => {
-        hideLoader();
+        
         showContent();
-    }, 1000); // Replace with your actual data loading logic and time
+    }, 500); // Replace with your actual data loading logic and time
 
-    function hideLoader() {
-        const loader = document.getElementById("loader");
-        document.getElementById("loader").classList.add("loaderani")
-
-    }
+    
 
     function showContent() {
         const content = document.getElementById("content");
