f1f941caba0137795baab98acb8bd3140f4471fb
lograz <103561745+lograz@users.noreply.github.com>
2024-05-21 14:36:17 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -17,3 +17,35 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 });
 
+const template = document.createElement('template');
+template.innerHTML = 
+`<style>
+.row {
+    width: 100%; display: flex; flex-direction: row; flex-wrap: wrap;justify-content: space-between; align-content: stretch; align-items: stretch;gap: 22px;}.col{flex-grow: 1;}ul {display: flex;list-style: none;gap: 16px;padding: 0;}li {font-size: 14px;font-weight: 700;}li a {color: #071a2b !important;}.row.footer {align-items: center;}.last-update{display: flex;align-items: center;justify-content: flex-end;flex-direction: row;}</style><div class="row footer"> <div class="col-8"> <span>Designed and Developed by me (Lorenzo Gravina) with Figma, HTML, CSS and JS</span></div> <div class="col-4 last-update"><ul><li><a href="https://www.linkedin.com/in/lorenzo-gravina-a4175037/">LINKEDIN</a></li> <li><a href="https://dribbble.com/lorenzogravina">DRIBBBLE</a></li> <li><a href="https://www.figma.com/@gravinalorenzo">FIGMA</a></li></ul></div> </div>`
+
+;
+
+class FooterCustom extends HTMLElement {
+    constructor(){
+        super();
+        this.attachShadow({mode: 'open'});
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+    }
+}
+
+window.customElements.define('footer-custom', FooterCustom)
+
+const cards = document.querySelectorAll(".card_due");
+const wrapper = document.querySelector(".cards_uno");
+
+wrapper.addEventListener("mousemove", function ($event) {
+	cards.forEach((card) => {
+		const rect = card.getBoundingClientRect();
+		const x = $event.clientX - rect.left;
+		const y = $event.clientY - rect.top;
+
+		card.style.setProperty("--xPos", `${x}px`);
+		card.style.setProperty("--yPos", `${y}px`);
+	});
+});
+
